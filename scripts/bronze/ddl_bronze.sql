3fedb83cec28fe9bab05a2c55431df70913c334a
K.H.H Sai Datta <ksaidatta.116@gmail.com>
2025-11-04 11:57:16 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -9,164 +9,54 @@ Script Purpose:
 =============================================================================
 */
 
-CREATE OR ALTER PROCEDURE bronze.load_bronze AS
-BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME,@batch_start_time DATETIME, @batch_end_time DATETIME;
-	BEGIN TRY
-		SET @batch_start_time = GETDATE();
-		PRINT '========================================';
-		PRINT 'Loading Bronze Layer';
-		PRINT '========================================';
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
 
-		PRINT'------------------------------------';
-		PRINT'Loading CRM Tables';
-		PRINT'------------------------------------';
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
 
+CREATE TABLE bronze.crm_sales_details (
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
 
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT'CRM_CUSTOMER_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
+CREATE TABLE bronze.erp_loc_a101 (
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
 
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.crm_cust_info'
-		TRUNCATE TABLE bronze.crm_cust_info;
-
-		PRINT '>> Inserting Data Into: bronze.crm_cust_info'
-		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-		SELECT COUNT(*) FROM bronze.crm_cust_info
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT 'CRM_PRODUCT_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-	
-		SET @start_time = GETDATE();
-	    PRINT '>> Truncating Table: bronze.crm_prd_info'
-		TRUNCATE TABLE bronze.crm_prd_info;
-
-		PRINT '>> Inserting Data Into: bronze.crm_prd_info'
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SELECT COUNT(*) FROM bronze.crm_prd_info
-		SET @end_time = GETDATE()
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
-		PRINT'+++++++++++++++++';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT'CRM_SALES_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.crm_prd_info'
-		TRUNCATE TABLE bronze.crm_prd_info;
-
-		PRINT '>> Inserting Data Into: bronze.crm_prd_info'
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SELECT COUNT(*) FROM bronze.crm_prd_info
-
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
-		PRINT '------------------------------------------------------';
-		PRINT 'Loading ERP Tables';
-		PRINT '------------------------------------------------------';
+CREATE TABLE bronze.erp_cust_az12 (
+cid NVARCHAR (50),
+bdate DATE,
+gen NVARCHAR (50)
+);
 
 
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT'ERP_CUST_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-	
-		SET @start_time = GETDATE();
-	    PRINT '>> Truncating Table: bronze.erp_cust_az12'
-		TRUNCATE TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze. erp_px_cat_g1v2 (
+id NVARCHAR(50),
+cat  NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
 
-		PRINT '>> Inserting Data Into: bronze.erp_cust_az12'
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_erp\CUST_AZ12.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SELECT COUNT(*) FROM bronze.erp_cust_az12
-
-		SELECT COUNT(*) FROM bronze.crm_cust_info
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
-
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT'ERP_LOC_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.erp_loc_a101'
-		TRUNCATE TABLE bronze.erp_loc_a101;
-
-		PRINT '>> Inserting Data Into: bronze.erp_loc_a101'
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_erp\LOC_A101.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SELECT COUNT(*) FROM bronze.erp_loc_a101
-
-		SELECT COUNT(*) FROM bronze.crm_cust_info
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+'seconds';
-		print '--------';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		PRINT 'ERP_LOC_TABLE';
-		PRINT'+++++++++++++++++++++++++++++++++++++';
-		
-		SET @start_time = GETDATE();
-		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2'
-		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		
-		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2'
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Data with Baara\sql-data-warehouse-project-main\datasets\source_erp\PX_CAT_G1V2.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-		SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
-		SELECT COUNT(*) FROM bronze.crm_cust_info
-		SET @end_time = GETDATE();
-		PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND,@start_time,@end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
-		END TRY
-	BEGIN CATCH
-		PRINT '=========================================='
-		PRINT 'ERROR OCCURED DURING LOADIN BRONZE LAYER'
-		PRINT 'Error Message:' + ERROR_MESSAGE();
-		PRINT 'Error Number:' + CAST (ERROR_NUMBER() AS NVARCHAR);
-		PRINT 'Error Number:' + CAST (ERROR_STATE() AS NVARCHAR);
-		PRINT '=========================================='
-	END CATCH
-END
-		SET @batch_end_time = GETDATE();
-		PRINT '>> Bronze Layer Load Duration:' + CAST(DATEDIFF(SECOND,@batch_start_time,@batch_end_time) AS NVARCHAR)+ 'seconds';
-		print '--------';
